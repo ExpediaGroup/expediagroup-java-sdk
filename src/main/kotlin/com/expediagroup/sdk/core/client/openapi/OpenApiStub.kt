75c887ca05b02031d44483b19201f25352c30bfe
Hamza <v-hqader@expediagroup.com>
2022-08-29 10:02:19 +0300

    fix: Solve empty response body issue

--- src/main/kotlin/com/expediagroup/sdk/core/client/openapi/OpenApiStub.kt
+++ src/main/kotlin/com/expediagroup/sdk/core/client/openapi/OpenApiStub.kt
@@ -17,10 +17,13 @@ package com.expediagroup.sdk.core.client.openapi
 
 import com.expediagroup.sdk.core.client.Client
 import com.expediagroup.sdk.core.exceptions.ServiceException
+import com.expediagroup.sdk.core.exceptions.errors.Error
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.request
+import io.ktor.http.toURI
 
 /**
  * Base class used by generated OpenAPI APIs to perform the heavy lifting of actually sending requests
@@ -49,10 +52,19 @@ abstract class OpenApiStub {
     protected suspend fun throwIfError(response: HttpResponse) {
         if (response.status.value !in 200..299) {
             // Make sure we read the body to avoid resource leaks
-            throw ServiceException(
-                response.status,
-                response.body()
-            )
+            runCatching {
+                response.body<Error>()
+            }.onSuccess {
+                throw ServiceException(response.status, it)
+            }.onFailure {
+                throw ServiceException(
+                    response.status,
+                    Error(
+                        response.request.url.toURI(),
+                        response.status.description
+                    )
+                )
+            }
         }
     }
 }
