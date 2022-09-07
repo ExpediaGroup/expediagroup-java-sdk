912d5438084ecad182b203eee1d63b349ba3c86f
mohnoor94 <moh.noor94@gmail.com>
2022-09-07 16:59:51 +0300

    feat(configurations): Support Configuration Providers
    Issue: https://github.com/ExpediaGroup/openworld-sdk-java-core/issues/8

---
+++ src/main/kotlin/com/expediagroup/sdk/core/configuration/collecter/ConfigurationCollector.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2022 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.expediagroup.sdk.core.configuration.collecter
+
+import com.expediagroup.sdk.core.configuration.provider.ConfigurationProvider
+
+/**
+ * Configuration collector that collects configuration from all available providers.
+ *
+ * @param providers A configuration providers queue.
+ */
+internal class ConfigurationCollector private constructor(providers: ConfigurationProviderQueue) : ConfigurationProvider {
+
+    companion object Factory {
+
+        /**
+         * Creates a new [ConfigurationCollector] with the given [providerQueue].
+         *
+         * @param providerQueue the [ConfigurationProviderQueue] to use.
+         * @return a new [ConfigurationCollector] with the given [providerQueue].
+         */
+        fun create(providerQueue: ConfigurationProviderQueue): ConfigurationCollector = ConfigurationCollector(providerQueue)
+
+        /**
+         * Creates a new [ConfigurationCollector] with the given [providers].
+         *
+         * @param providers the [ConfigurationProvider]s to use.
+         * @return a new [ConfigurationCollector] with the given [providers].
+         */
+        fun create(vararg providers: ConfigurationProvider): ConfigurationCollector {
+            return create(ConfigurationProviderQueue.from(providers.asList()))
+        }
+    }
+
+    override val key: String by lazy { providers.firstOf { it.key } }
+    override val secret: String by lazy { providers.firstOf { it.secret } }
+    override val endpoint: String by lazy { providers.firstOf { it.endpoint } }
+    override val authEndpoint: String by lazy { providers.firstOf { it.authEndpoint } }
+}
---
+++ src/test/kotlin/com/expediagroup/sdk/core/configuration/collecter/ConfigurationProviderQueueTest.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2022 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.expediagroup.sdk.core.configuration.collecter
+
+import com.expediagroup.sdk.core.commons.TestConstants.CLIENT_KEY_TEST_CREDENTIAL
+import com.expediagroup.sdk.core.commons.TestConstants.CLIENT_SECRET_TEST_CREDENTIAL
+import com.expediagroup.sdk.core.configuration.provider.DefaultConfigurationProvider
+import com.expediagroup.sdk.core.configuration.provider.RuntimeConfigurationProvider
+import com.expediagroup.sdk.core.constants.ClientConstants.DEFAULT_AUTH_ENDPOINT
+import com.expediagroup.sdk.core.constants.ClientConstants.DEFAULT_ENDPOINT
+import com.expediagroup.sdk.core.constants.ClientConstants.EMPTY_STRING
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class ConfigurationProviderQueueTest {
+
+    @Test
+    fun `test a provider queue with only a default provider`() {
+        val configurationProviderQueue = ConfigurationProviderQueue.from(listOf(DefaultConfigurationProvider))
+        assertEquals(EMPTY_STRING, configurationProviderQueue.firstOf { it.key })
+        assertEquals(EMPTY_STRING, configurationProviderQueue.firstOf { it.secret })
+        assertEquals(DEFAULT_ENDPOINT, configurationProviderQueue.firstOf { it.endpoint })
+        assertEquals(DEFAULT_AUTH_ENDPOINT, configurationProviderQueue.firstOf { it.authEndpoint })
+    }
+
+    @Test
+    fun `test a provider queue with at least a non-empty provider`() {
+        val runtimeConfigurationProvider = RuntimeConfigurationProvider(
+            key = CLIENT_KEY_TEST_CREDENTIAL,
+            secret = CLIENT_SECRET_TEST_CREDENTIAL,
+            endpoint = DEFAULT_ENDPOINT,
+            authEndpoint = DEFAULT_AUTH_ENDPOINT
+        )
+        val configurationProviderQueue = ConfigurationProviderQueue.from(
+            listOf(runtimeConfigurationProvider)
+        )
+
+        assertEquals(runtimeConfigurationProvider, configurationProviderQueue.first())
+        assertEquals(runtimeConfigurationProvider, configurationProviderQueue.first { it.key != null })
+        assertEquals(runtimeConfigurationProvider, configurationProviderQueue.first { it.key != EMPTY_STRING })
+
+        assertEquals(configurationProviderQueue.firstOf { it.key }, CLIENT_KEY_TEST_CREDENTIAL)
+        assertEquals(configurationProviderQueue.firstOf { it.secret }, CLIENT_SECRET_TEST_CREDENTIAL)
+        assertEquals(configurationProviderQueue.firstOf { it.endpoint }, DEFAULT_ENDPOINT)
+        assertEquals(configurationProviderQueue.firstOf { it.authEndpoint }, DEFAULT_AUTH_ENDPOINT)
+    }
+}
