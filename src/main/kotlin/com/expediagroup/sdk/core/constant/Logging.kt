bd8bdc61f70f2c5a4303d9b9cc9a3de901bbe38b
Osama Salman <osama.salman99@gmail.com>
2022-10-12 17:09:47 +0300

    chore: add logging for Authentication

--- src/main/kotlin/com/expediagroup/sdk/core/constant/Logging.kt
+++ src/main/kotlin/com/expediagroup/sdk/core/constant/Logging.kt
@@ -20,7 +20,7 @@ object Logging {
     const val SUCCESSFUL_TOKEN_RENEWAL = "Token renewal successful: New token expires in {} seconds"
     const val FAILED_TOKEN_RENEWAL = "Token renewal failed: Response status[{}]"
     const val CLEARING_TOKENS = "Clearing tokens"
-    const val TOKEN_EXPIRED = "Token expired or is about to expire: Requests will wait until token is renewed"
+    const val TOKEN_EXPIRED = "Token expired or is about to expire: Request will wait until token is renewed"
     const val UNSUCCESSFUL_RESPONSE = "Unsuccessful response [{}]"
     const val COULD_NOT_RECEIVE_RESPONSE_PAYLOAD = "Could not receive the payload of the response"
 }
