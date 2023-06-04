66c3a4c5731629af0c52baaf19b299f865ef2f9c
Osama Salman <osama.salman99@gmail.com>
2023-06-04 18:11:19 +0300

    feat(generator): Assign Discriminator Values Internally
    Assign discriminator values internally

--- generator/openapi/src/main/kotlin/com/expediagroup/openworld/sdk/generators/openapi/MustacheHelpers.kt
+++ generator/openapi/src/main/kotlin/com/expediagroup/openworld/sdk/generators/openapi/MustacheHelpers.kt
@@ -16,9 +16,61 @@
 package com.expediagroup.openworld.sdk.generators.openapi
 
 import com.samskivert.mustache.Mustache
+import org.openapitools.codegen.CodegenModel
+import org.openapitools.codegen.CodegenProperty
 
 val mustacheHelpers = mapOf(
     "removeLeadingSlash" to {
         Mustache.Lambda { fragment, writer -> writer.write(fragment.execute().removePrefix("/")) }
+    },
+    "assignDiscriminators" to {
+        Mustache.Lambda { fragment, writer ->
+            val model = fragment.context() as CodegenModel
+            getParentDiscriminator(model).forEach {
+                val type = (if (it.isEnum) "${it.parentName}." else "") + it.type
+                writer.write("@JsonProperty(\"${it.originalName}\")\n")
+                writer.write("override val ${it.name} : $type = $type.${it.value}\n")
+            }
+        }
+    },
+    "eliminateDiscriminators" to {
+        Mustache.Lambda { fragment, writer ->
+            val discriminators: List<String> = getParentDiscriminator(fragment.context(1) as CodegenModel).map { it.originalName }
+            val property = fragment.context() as CodegenProperty
+            if (!discriminators.contains(property.baseName)) {
+                writer.write(fragment.execute())
+            }
+        }
     }
+)
+
+private fun getParentDiscriminator(model: CodegenModel): MutableList<Discriminator> {
+    val discriminators: MutableList<Discriminator> = model.parent?.let { getParentDiscriminator(model.parentModel) } ?: mutableListOf()
+    model.parentModel?.vars?.find { it.isDiscriminator }?.let { variable ->
+        model.parentModel?.discriminator?.let {
+            discriminators.add(
+                Discriminator(
+                    it.propertyBaseName,
+                    it.propertyName,
+                    variable.datatypeWithEnum,
+                    it.mappedModels.find { mappedModel ->
+                        println(model.classname)
+                        mappedModel.modelName.equals(model.classname)
+                    }!!.mappingName,
+                    model.parentModel.name,
+                    variable.isEnum
+                )
+            )
+        }
+    }
+    return discriminators
+}
+
+private data class Discriminator(
+    val originalName: String,
+    val name: String,
+    val type: String,
+    val value: String,
+    val parentName: String,
+    val isEnum: Boolean
 )
