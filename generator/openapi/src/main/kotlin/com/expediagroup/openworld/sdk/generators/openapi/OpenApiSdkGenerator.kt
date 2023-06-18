c3df6f758ea6056ad59abd8a64af84bdd6146454
Mohammad Noor Abu Khleif <moh.noor94@gmail.com>
2023-06-18 18:30:04 +0300

    chore: Deprecate Spec Processing Logic (#221)

--- generator/openapi/src/main/kotlin/com/expediagroup/openworld/sdk/generators/openapi/OpenApiSdkGenerator.kt
+++ generator/openapi/src/main/kotlin/com/expediagroup/openworld/sdk/generators/openapi/OpenApiSdkGenerator.kt
@@ -15,7 +15,6 @@
  */
 package com.expediagroup.openworld.sdk.generators.openapi
 
-import com.expediagroup.openworld.sdk.generators.openapi.processor.YamlProcessor
 import com.expediagroup.openworld.sdk.model.ClientGenerationException
 import com.expediagroup.openworld.sdk.product.Product
 import com.expediagroup.openworld.sdk.product.ProductFamily
@@ -27,13 +26,6 @@ import org.openapitools.codegen.CodegenConstants
 import org.openapitools.codegen.DefaultGenerator
 import org.openapitools.codegen.SupportingFile
 import org.openapitools.codegen.config.CodegenConfigurator
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.nio.file.Files
-import java.util.Base64
-import java.util.zip.ZipInputStream
-import kotlin.io.path.writeBytes
 
 /**
  * Configures the OpenAPI Generator based on command line parameters to generate an EG Travel SDK project
@@ -81,12 +73,9 @@ class OpenApiSdkGenerator {
         try {
             val product = Product(namespace, programmingLanguage)
             val config = CodegenConfigurator().apply {
-                val path = prepareSpecFile()
-                val processedFilePath = preProcessSpecFile(path)
-
                 setGeneratorName("kotlin")
                 setTemplateDir("templates/openworld-sdk")
-                setInputSpec(processedFilePath)
+                setInputSpec(inputFile)
                 setOutputDir(outputDirectory)
                 setArtifactId(product.artifactId)
                 setArtifactVersion(version)
@@ -146,31 +135,5 @@ class OpenApiSdkGenerator {
         } catch (e: Exception) {
             throw ClientGenerationException("Failed to generate SDK", e)
         }
-    }
-
-    private fun preProcessSpecFile(path: String): String {
-        val yamlProcessor = YamlProcessor(path, namespace)
-        return yamlProcessor.process()
-    }
-
-    private fun prepareSpecFile(): String {
-        val buffer = ByteArray(1024)
-        val zipInputStream = ZipInputStream(FileInputStream(prepareTmpZipFile()))
-        val tempFile = Files.createTempFile("", zipInputStream.nextEntry?.name).toFile()
-        val fileOutputStream = FileOutputStream(tempFile)
-        var len: Int
-        while (zipInputStream.read(buffer).also { len = it } > 0) {
-            fileOutputStream.write(buffer, 0, len)
-        }
-        zipInputStream.closeEntry()
-        fileOutputStream.close()
-        zipInputStream.close()
-        return tempFile.absolutePath
-    }
-
-    private fun prepareTmpZipFile(): File {
-        val tmpFile = Files.createTempFile("", "tmp")
-        tmpFile.writeBytes(Base64.getDecoder().decode(inputFile))
-        return tmpFile.toFile()
     }
 }
