80b5ae8dc51e3f8047857b3c90ed88d93f82ed10
Expedia Group Open Source <oss@expediagroup.com>
2023-07-11 14:10:35 +0000

    chore: Publish fraudPreventionV2 [1.2.0] SDK

---
+++ release/fraudPreventionV2/src/main/kotlin/com/expediagroup/openworld/sdk/fraudpreventionv2/models/exception/PropertyConstraintViolation.kt
@@ -0,0 +1,14 @@
+package com.expediagroup.openworld.sdk.fraudpreventionv2.models.exception
+
+/**
+ * An entity to represent a constraint violation of a property.
+ *
+ * @property name The name of the constraint-violated field
+ * @property path The path of the constraint-violated field
+ * @property message The constraint violation message
+ */
+data class PropertyConstraintViolation(
+    val name: String,
+    val path: String,
+    val message: String
+)
