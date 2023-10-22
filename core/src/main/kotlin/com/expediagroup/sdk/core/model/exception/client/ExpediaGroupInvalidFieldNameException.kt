f7a753cbbed3c526644a465a039c75e0535c7068
Osama Salman <osama.salman99@gmail.com>
2023-10-22 14:47:27 +0300

    feat(logging): Field Masking in Logging
    Add ability to include more fields to be masked in logging

--- core/src/main/kotlin/com/expediagroup/sdk/core/model/exception/client/ExpediaGroupInvalidFieldNameException.kt
+++ core/src/main/kotlin/com/expediagroup/sdk/core/model/exception/client/ExpediaGroupInvalidFieldNameException.kt
@@ -18,7 +18,7 @@ package com.expediagroup.sdk.core.model.exception.client
 /**
  * Thrown to indicate that one or more passed field names are invalid.
  *
- * @param fieldsNames the names of the invalid fields.
+ * @param invalidFields the names of the invalid fields.
  */
-class ExpediaGroupInvalidFieldNameException(fieldsNames: Collection<String>) :
-    ExpediaGroupClientException("All fields names must contain only alphanumeric characters in addition to - and _ but found [${fieldsNames.joinToString(",")}]")
+class ExpediaGroupInvalidFieldNameException(invalidFields: Collection<String>) :
+    ExpediaGroupClientException("All fields names must contain only alphanumeric characters in addition to - and _ but found [${invalidFields.joinToString(",")}]")
