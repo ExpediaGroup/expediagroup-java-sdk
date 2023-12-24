719e9ec6a8758e51bfb35ed13f31a8f074ae02c5
Mohammad Noor Abu Khleif <moh.noor94@gmail.com>
2023-12-24 17:12:09 +0300

    chore: update type enum commit rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
         'header-max-length': [2, 'always', 250],
         'type-empty': [2, 'never'],
         'type-case': [2, 'always', 'lower-case'],
-        'type-enum': [2, 'always', ['feat', 'refactor', 'fix', 'docs', 'test', 'chore', 'ci', 'release']],
+        'type-enum': [2, 'always', ['feat', 'refactor', 'fix', 'docs', 'test', 'chore', 'ci', 'release', 'build']],
         'subject-empty': [2, 'never'],
         'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'kebab-case', 'lower-case']],
         'body-leading-blank': [2, 'always'],
