6532379ae1dc38f8f738e27fdc13d7cbbcbef24f
Osama Salman <osama.salman99@gmail.com>
2024-01-11 15:09:29 +0300

    fix: fix 3

--- update-github-release.sh
+++ update-github-release.sh
@@ -4,4 +4,8 @@ tags=$(git tag -l "v*")
 latest_tag=$(echo "$tags" | grep -E '^v[0-9]+$' | sort -Vr | head -n 1)
 today="v$(date "+%Y%m%d%H%M%S")"
 
+echo "tags: $tags"
+echo "latest: $latest_tag"
+echo "today: $today"
+
 gh release create "$today" --draft --generate-notes --latest --notes-start-tag "$latest_tag" -title "$today"
