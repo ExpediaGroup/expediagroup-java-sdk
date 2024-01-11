1fc35bbef986f97d65ad7d781ad424a582260ae6
Osama Salman <osama.salman99@gmail.com>
2024-01-11 14:25:17 +0300

    fix: another fix

--- update-github-release.sh
+++ update-github-release.sh
@@ -2,6 +2,6 @@
 
 nextVersion=$1
 today=$(date "+%Y%m%d%H%M%S")
-git tag v"$today" "$nextVersion"
-git push --tags
-gh release edit "$nextVersion" --title v"$today" --tag v"$today"
+#git tag v"$today" "$nextVersion"
+#git push --tags
+#gh release edit "$nextVersion" --title v"$today" --tag v"$today"
