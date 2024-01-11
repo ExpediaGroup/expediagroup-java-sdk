dd95c008364fe974d8cb36e3923d35b398792cf9
Osama Salman <osama.salman99@gmail.com>
2024-01-11 13:27:50 +0300

    chore: some changes

--- update-github-release.sh
+++ update-github-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 nextVersion=$1
-today=$(date "+%Y%m%d")
+today=$(date "+%Y%m%d%H%M%S")
 git tag v"$today" "$nextVersion"
 git push --tags
 gh release edit "$nextVersion" --title v"$today" --tag v"$today"
