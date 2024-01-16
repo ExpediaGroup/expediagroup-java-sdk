01b9a8456d1818d7f2d4f81384112cd35f4e60db
Osama Salman <osama.salman99@gmail.com>
2024-01-16 14:23:14 +0300

    feat: some change

--- update-github-release.sh
+++ update-github-release.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-today="v$(date "+%Y%m%d%H%M%S")"
-
-echo "today: $today"
-
-git tag "$today"
+nextVersion=$1
+today=$(date "+%Y%m%d")
+git tag v"$today" "$nextVersion"
 git push --tags
-gh release create "$today" --draft --generate-notes --latest --notes-file release-notes.txt --title "$today"
-
-git push origin --delete v1.0.0 v1.0.1 v1.1.0 v2.0.0
+gh release edit "$nextVersion" --title v"$today" --tag v"$today"
