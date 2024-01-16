c2adba6405a0f118e8db5486b8d2731e58c24817
Osama Salman <osama.salman99@gmail.com>
2024-01-16 11:58:04 +0300

    feat: some change

--- update-github-release.sh
+++ update-github-release.sh
@@ -8,4 +8,4 @@ git tag "$today"
 git push --tags
 gh release create "$today" --draft --generate-notes --latest --notes-file release-notes.txt --title "$today"
 
-git push --delete v1.0.0
+git push origin --delete v1.0.0 v1.0.1 v1.1.0 v2.0.0
