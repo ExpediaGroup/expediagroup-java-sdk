36b73cd3e50d85a8363c1e86870db41c6917c66d
Osama Salman <osama.salman99@gmail.com>
2024-02-08 13:48:50 +0300

    fix: rapid last paginated response (#430)
    Checks whether the paginated response is not empty and returns an empty collection otherwise.

--- core/src/main/kotlin/com/expediagroup/sdk/core/plugin/logging/ResponseBodyLogger.kt
+++ core/src/main/kotlin/com/expediagroup/sdk/core/plugin/logging/ResponseBodyLogger.kt
@@ -57,7 +57,7 @@ class ResponseBodyLogger {
     }
 }
 
-private fun HttpResponse.contentEncoding(): String? = headers[HttpHeaders.ContentEncoding]
+fun HttpResponse.contentEncoding(): String? = headers[HttpHeaders.ContentEncoding]
 
 internal object GZipEncoder : ContentEncoder, Encoder by GZip {
     override val name: String = HeaderValue.GZIP
