1d406bde8f7891f08b567ce086f47c763e222195
Omar Al-Jarrah <oaljarrah@expediagroup.com>
2024-08-01 17:20:12 +0300

    chore: add kotlin docs

--- generator/openapi/src/main/kotlin/com/expediagroup/sdk/generators/openapi/Extensions.kt
+++ generator/openapi/src/main/kotlin/com/expediagroup/sdk/generators/openapi/Extensions.kt
@@ -1,5 +1,13 @@
 package com.expediagroup.sdk.generators.openapi
 
+/**
+ * This extension function converts a string to PascalCase.
+ * PascalCase is a type of identifier that consists of compound words or phrases such that each word or abbreviation begins with a capital letter.
+ * Non-alphabetic characters are ignored, and the next alphabetic character after them is capitalized.
+ *
+ * @receiver String The string to be converted to PascalCase.
+ * @return String The string converted to PascalCase.
+ */
 fun String.pascalCase(): String {
     var capitalizeNext = true
     val builder = StringBuilder()
