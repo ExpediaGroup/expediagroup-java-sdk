38539f6fc2550f3a37859a3c1b9582de7f67c009
Nasser Anssari <nanssari@expediagroup.com>
2024-09-17 15:49:08 +0300

    Add cars endpoints

--- generator/openapi/src/main/resources/post-processor/processor.ts
+++ generator/openapi/src/main/resources/post-processor/processor.ts
@@ -1,18 +1,37 @@
+import {processCarCancellationPolicy} from "./processors/car-cancellation-policy";
 import { processGetLodgingListingsOperationParams } from './processors/get-lodging-listings-operation-params'
 import { processGetLodgingQuotesOperationParams } from "./processors/get-lodging-quotes-operation-params";
+import { processNonCancellableDateTimeRange } from "./processors/non-cancellable-date-time-range";
+import { processPenaltyRule } from "./processors/penalty-rule"
+import { processVendorLocationDetails} from "./processors/vendor-location-details";
+
 import { processTraderDetailsInner } from './processors/trader-details-inner'
+
 import * as path from 'path'
+
 
 const args = process.argv.slice(2)
 const filePath = args[0]
 const fileName = path.parse(filePath).name
 
 switch (fileName) {
+    case 'CarCancellationPolicy':
+        processCarCancellationPolicy(filePath)
+        break
     case 'GetLodgingListingsOperationParams':
         processGetLodgingListingsOperationParams(filePath)
         break
     case 'GetLodgingQuotesOperationParams':
         processGetLodgingQuotesOperationParams(filePath)
+        break
+    case 'NonCancellableDateTimeRange':
+        processNonCancellableDateTimeRange(filePath)
+        break
+    case 'PenaltyRule':
+        processPenaltyRule(filePath)
+        break
+    case 'VendorLocationDetails':
+        processVendorLocationDetails(filePath)
         break
     case 'TraderDetailsInner':
         processTraderDetailsInner(filePath)
---
+++ generator/openapi/src/main/resources/post-processor/processors/vendor-location-details.ts
@@ -0,0 +1,37 @@
+import * as fs from 'node:fs'
+import { Edit, Lang, NapiConfig, parse, SgNode } from '@ast-grep/napi'
+import { PathOrFileDescriptor } from 'fs'
+import * as yaml from 'yaml'
+
+function process (filePath: PathOrFileDescriptor): void {
+    const source = fs.readFileSync(filePath, 'utf-8')
+    const ast = parse(Lang.Kotlin, source)
+    const root = ast.root()
+
+    const edits = [changeClassParamType, changeBuilderMethodParamType].flatMap((func) => func(root))
+    const newSource = root.commitEdits(edits)
+    fs.writeFileSync(filePath, newSource)
+}
+
+function readRule (ruleName: string): NapiConfig {
+    const rule = fs.readFileSync(`./rules/vendor-location-details/${ruleName}.yaml`, 'utf-8')
+    return yaml.parse(rule)
+}
+
+function changeClassParamType(root: SgNode) : Edit[] {
+    const config = readRule("change-class-param-type")
+
+    return root.findAll(config).map((node) => {
+        return node.replace("java.time.LocalDateTime")
+    })
+}
+
+function changeBuilderMethodParamType(root: SgNode) : Edit[] {
+    const config = readRule("change-builder-method-param-type")
+
+    return root.findAll(config).map((node) => {
+        return node.replace("java.time.LocalDateTime")
+    })
+}
+
+export { process as processVendorLocationDetails }
