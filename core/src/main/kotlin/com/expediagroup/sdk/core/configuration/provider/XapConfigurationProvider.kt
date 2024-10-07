d95b56f850bc1e0dd73562b4e1c79bb07f0e59eb
Nasser Anssari <nasser.anssari@gmail.com>
2024-10-07 16:46:19 +0300

    feat: add EWS client and config classes (#773)

---
+++ core/src/main/kotlin/com/expediagroup/sdk/core/configuration/provider/XapConfigurationProvider.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2022 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.expediagroup.sdk.core.configuration.provider
+
+import com.expediagroup.sdk.core.configuration.provider.RapidConfigurationProvider.connectionTimeout
+import com.expediagroup.sdk.core.configuration.provider.RapidConfigurationProvider.endpoint
+import com.expediagroup.sdk.core.configuration.provider.RapidConfigurationProvider.name
+import com.expediagroup.sdk.core.configuration.provider.RapidConfigurationProvider.requestTimeout
+import com.expediagroup.sdk.core.configuration.provider.RapidConfigurationProvider.socketTimeout
+import com.expediagroup.sdk.core.constant.Constant
+
+/**
+ * Default configuration provider for Rapid.
+ *
+ * @property name The name of the provider.
+ * @property endpoint The API endpoint to use for requests.
+ * @property requestTimeout The API response timeout to use for requests.
+ * @property connectionTimeout The connection timeout to use for requests.
+ * @property socketTimeout The socket timeout to use for requests.
+ */
+internal object XapConfigurationProvider : ConfigurationProvider {
+    override val name: String = "XAP Configuration Provider"
+    override val endpoint: String = "https://apim.expedia.com"
+    override val requestTimeout: Long = Constant.INFINITE_TIMEOUT
+    override val connectionTimeout: Long = Constant.TEN_SECONDS_IN_MILLIS
+    override val socketTimeout: Long = Constant.FIFTEEN_SECONDS_IN_MILLIS
+}
