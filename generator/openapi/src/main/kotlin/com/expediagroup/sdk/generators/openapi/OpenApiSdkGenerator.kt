7e5a5d3132c33293730cd7ff38471505ed935f70
Nasser Anssari <nasser.anssari@gmail.com>
2024-11-25 17:40:32 +0300

    feat: validate operation params (#864)

--- generator/openapi/src/main/kotlin/com/expediagroup/sdk/generators/openapi/OpenApiSdkGenerator.kt
+++ generator/openapi/src/main/kotlin/com/expediagroup/sdk/generators/openapi/OpenApiSdkGenerator.kt
@@ -38,10 +38,7 @@ class OpenApiSdkGenerator {
         mutableListOf(
             "pom.xml",
             "README.md",
-            "ApiException.kt",
-            "PropertyConstraintViolation.kt",
-            "PropertyConstraintsValidator.kt",
-            "PropertyConstraintViolationException.kt"
+            "ApiException.kt"
         )
 
     companion object {
@@ -141,27 +138,6 @@ class OpenApiSdkGenerator {
                                     "models/apiException.mustache",
                                     "$packagePath/models/exception/",
                                     "ApiException.kt"
-                                )
-                            )
-                            add(
-                                SupportingFile(
-                                    "validation/propertyConstraintViolationException.mustache",
-                                    "$packagePath/models/exception/",
-                                    "PropertyConstraintViolationException.kt"
-                                )
-                            )
-                            add(
-                                SupportingFile(
-                                    "validation/propertyConstraintViolation.mustache",
-                                    "$packagePath/models/exception/",
-                                    "PropertyConstraintViolation.kt"
-                                )
-                            )
-                            add(
-                                SupportingFile(
-                                    "validation/propertyConstraintsValidator.mustache",
-                                    "$packagePath/validation/",
-                                    "PropertyConstraintsValidator.kt"
                                 )
                             )
 
