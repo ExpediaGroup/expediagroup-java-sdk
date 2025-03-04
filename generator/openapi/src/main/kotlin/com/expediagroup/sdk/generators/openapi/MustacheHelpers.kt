1b45a81ab9013121e47b6a06af6fdbe68c5efa77
OmarAlJarrah <o.mazari.om63@gmail.com>
2025-03-04 15:47:43 +0300

    chore: remove unneeded tests

--- generator/openapi/src/main/kotlin/com/expediagroup/sdk/generators/openapi/MustacheHelpers.kt
+++ generator/openapi/src/main/kotlin/com/expediagroup/sdk/generators/openapi/MustacheHelpers.kt
@@ -147,15 +147,17 @@ val mustacheHelpers =
                 fragment.execute(context, writer)
             }
         },
-        "allowedMediaTypes" to Mustache.Lambda { fragment, writer ->
-            val response: CodegenResponse = fragment.context() as CodegenResponse
-            if (response.is2xx) {
-                val mediaTypes: List<String> = response.content.keys.filter {
-                    !it.contains("xml", ignoreCase = true)
-                }
+        "allowedMediaTypes" to {
+            Mustache.Lambda { fragment, writer ->
+                val response: CodegenResponse = fragment.context() as CodegenResponse
+                if (response.is2xx) {
+                    val mediaTypes: List<String> = response.content.keys.filter {
+                        !it.contains("xml", ignoreCase = true)
+                    }
 
-                val context = mapOf("mediaTypes" to mediaTypes)
-                fragment.execute(context, writer)
+                    val context = mapOf("mediaTypes" to mediaTypes)
+                    fragment.execute(context, writer)
+                }
             }
         }
     )
