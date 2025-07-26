565e7e30a77eac47c785171e16b03ea4672de20e
mdwairi <mdwairi@expediagroup.com>
2025-07-26 10:02:05 +0300

    chore: reformat

--- exemplar/exemplar-playground-java/src/main/java/com/expediagroup/sdk/exemplar/playground/graphql/FullExemplarScenario.java
+++ exemplar/exemplar-playground-java/src/main/java/com/expediagroup/sdk/exemplar/playground/graphql/FullExemplarScenario.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2025 Expedia, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.expediagroup.sdk.exemplar.playground.graphql;
 
 import com.expediagroup.sdk.exemplar.graphql.client.ExemplarGraphQLClient;
@@ -164,7 +179,6 @@ public class FullExemplarScenario {
             .guestInfo(guestInfo)                      // Attach guest information
             .guests(3)                                 // Number of guests
             .build();
-
 
         CreateBookingMutation mutation = new CreateBookingMutation(input);
 
